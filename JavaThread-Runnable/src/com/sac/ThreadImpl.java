36ea687a9642df859c89dc5e65390bfa1dc57368
Sachin Naik <srnaik@hotmail.com>
2016-05-22 21:00:46 -0500

    Thread Runnablel Interface Example

---
+++ JavaThread-Runnable/src/com/sac/ThreadImpl.java
@@ -0,0 +1,43 @@
+package com.sac;
+
+public class ThreadImpl implements Runnable{
+	
+	private String threadName;
+	
+	public ThreadImpl(String threadName) {
+		this.threadName = threadName;
+	}
+
+	@Override
+	public void run() {
+		
+		for(int i = 4; i > 0; i--){
+			System.out.println("Thread Name: " + threadName + " " + i);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		
+		ThreadImpl threadImpl = new ThreadImpl("Custom Thread");
+		Thread thread = new Thread(threadImpl, threadImpl.getThreadName());
+		thread.start();
+		
+		
+	}
+
+	public String getThreadName() {
+		return threadName;
+	}
+
+	public void setThreadName(String threadName) {
+		this.threadName = threadName;
+	}
+
+}
